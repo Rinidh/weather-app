9ea94964802987a9bf3842accf88f928b2d66498
Rinidh <rinidhdhokia@gmail.com>
2025-02-24 15:56:05 +0300

    Initial commit. Theme color config, CSS resets, Basic HTML components & sun icon (asset)

---
+++ theme.js
@@ -0,0 +1,23 @@
+//can place this script in the <head> sec, as it will only run after DOM-content-loaded
+//alternatively, use the css version: css-code/theme.css
+
+document.addEventListener("DOMContentLoaded", () => {
+  const body = document.getElementsByTagName("body")[0]
+  const style = body.style
+
+  function addTheme() {
+    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      style.backgroundColor = "black"
+      style.color = "white"
+
+    } else {
+      style.backgroundColor = "rgb(241, 241, 241)";
+      style.color = "black"
+    }
+  }
+  addTheme()
+
+  // observe & adapt to theme changes automatically
+  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", addTheme)
+
+})
