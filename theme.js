9addd9dd5824058a6c7fa5c577320d4a0a649345
Rinidh <rinidhdhokia@gmail.com>
2025-02-24 17:13:05 +0300

    Style the top-banner / first section on 2 screen sizes (breakpoint at 700px)

--- theme.js
+++ theme.js
@@ -1,5 +1,6 @@
 //can place this script in the <head> sec, as it will only run after DOM-content-loaded
 //alternatively, use the css version: css-code/theme.css
+//since this file runs after DOM-content-loaded, it will always override the background-color & color styles set in the css files
 
 document.addEventListener("DOMContentLoaded", () => {
   const body = document.getElementsByTagName("body")[0]
